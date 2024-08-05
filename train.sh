efcba5cd9348b8f9bc0bbe499ab3367ad1b34bcc
woaixuexixuexi <2250460243@qq.com>
2024-08-05 22:02:32 +0800

    update ourmodel

---
+++ train.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+PARTITION=Segmentation
+
+GPU_ID=0 #,1,2,3
+dataset=coco # pascal coco
+exp_name=split0
+
+arch=ourmodel
+net=resnet50 # vgg resnet50
+
+exp_dir=exp/${dataset}/${arch}/${exp_name}/${net}
+snapshot_dir=${exp_dir}/snapshot
+result_dir=${exp_dir}/result
+config=config/${dataset}/${dataset}_${exp_name}_${net}.yaml
+mkdir -p ${snapshot_dir} ${result_dir}
+now=$(date +"%Y%m%d_%H%M%S")
+cp train.sh train.py ${config} ${exp_dir}
+
+echo ${arch}
+echo ${config}
+
+# CUDA_VISIBLE_DEVICES=${GPU_ID} python -m torch.distributed.launch --nproc_per_node=2 --master_port=1234 train.py \
+#         --config=${config} \
+#         --arch=${arch} \
+#         2>&1 | tee ${result_dir}/train-$now.log
+
+CUDA_VISIBLE_DEVICES=${GPU_ID} python -u train.py \
+       --config=${config} \
+       --arch=${arch} \
+       2>&1 | tee ${result_dir}/train-$now.log
+
+#Turn on tensorboardX recording
+#--viz \
+
